3e9b9c2a01e0b153c9d680952e22fb495cfcbeda
hughes#73 <57627511+HughesZhang73@users.noreply.github.com>
2020-07-11 22:01:15 +0800

    Python-OpenCV

---
+++ Python-Foundation/Moudle-Use/hello.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+def func():
+    """
+    自定义hello函数
+    :return:
+    """
+    print("hello")
+    
+    
+def add(int: num1, int: num2):
+    """
+    inr
+    :param int:
+    :return:
+    """
+    return 'hello'
+
+
+print(type(add(1, 2)))
---
+++ Python-Foundation/Python Overview/01 First meet Python.py
@@ -0,0 +1,3 @@
+print("hello python!")
+print("hello world")
+print("hello python1!")
